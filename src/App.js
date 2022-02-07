b3c596fd81f0a825cd5f9bc4a9f23a8b427eeb0f
ToddComte <comtetodd@gmail.com>
2022-02-07 10:03:49 -0700

    added the main title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Title">
+      <h1 className="Line1">Meals</h1>
+      <h2 className="Line2">Delicious Meals Crafted With Your Goals In Mind</h2>
+      <h2 className="Line3">
+        Order By Thursday For Our Wednesday Delivery or Pick Up At A Loction
+        Near You
+      </h2>
     </div>
   );
 }
